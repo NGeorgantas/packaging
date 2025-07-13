c777bf72510bec64bdc424fdd9423a3955741910
Nikos Georgantas <Nikos.Georgantas@witside.com>
2025-07-13 12:47:03 +0300

    test(test_pycounts): added plot_words test that is parameterized

--- pycounts/tests/test_pycounts.py
+++ pycounts/tests/test_pycounts.py
@@ -1,5 +1,8 @@
 from pycounts.pycounts import count_words
 from collections import Counter
+from pycounts.plotting import plot_words
+import matplotlib
+import pytest
 
 
 def test_count_words():
@@ -10,3 +13,32 @@ def test_count_words():
                         'different': 1, 'results': 1})
     actual = count_words("tests/einstein.txt")
     assert actual == expected, "Einstein quote counted incorrectly!"
+
+
+def test_plot_words():
+
+    """Test plotting of word counts."""
+
+    counts = Counter({'insanity': 1, 'is': 1, 'doing': 1,
+                      'the': 1, 'same': 1, 'thing': 1,
+                      'over': 2, 'and': 2, 'expecting': 1,
+                      'different': 1, 'results': 1})
+    fig = plot_words(counts)
+    assert isinstance(fig, matplotlib.container.BarContainer), \
+           "Wrong plot type"
+    assert len(fig.datavalues) == 10, \
+           "Incorrect number of bars plotted"
+
+
+@pytest.mark.parametrize(
+    "obj",
+    [
+        3.141,
+        "test.txt",
+        ["list", "of", "words"]
+    ]
+)
+def test_plot_words_error(obj):
+    """Check TypeError raised when Counter not used."""
+    with pytest.raises(TypeError):
+        plot_words(obj)
